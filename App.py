caaab6a34a3f071319222ec1f70d916187239ecd
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-23 16:28:20 +0800

    Update App.py

--- App.py
+++ App.py
@@ -17,9 +17,9 @@ def note():
   st.title("Note")
   st.subheader("What is Imbalanced Data?")
   st.markdown("Imbalanced data is a common problem in machine learning where the classes in a dataset are not represented equally. In fraud detection, for example, the number of instances of fraud may be significantly less than the number of instances of non-fraud, leading to a biased model that is less effective at detecting fraud. There are several techniques for addressing imbalanced data, including oversampling the minority class, undersampling the majority class, and using techniques such as SMOTE (Synthetic Minority Over-sampling Technique) and ADASYN (Adaptive Synthetic Sampling). The most appropriate method will depend on the specific dataset and the problem being solved. Some reference papers on imbalanced data are:")
-  st.markdown("He, H. and Garcia, E. A. (2008). Learning from imbalanced data. IEEE Transactions on Knowledge and Data Engineering, 20(1), pp.1263-1284.")
-  st.markdown("Chawla, N. V., Bowyer, K. W., Hall, L. O. and Kegelmeyer, W. P. (2002). SMOTE: Synthetic Minority Over-sampling Technique. Journal of Artificial Intelligence Research, 16, pp.321-357.")
-  st.markdown("Han, H., Wang, W. and Mao, B. (2005). Borderline-SMOTE: A new over-sampling method in imbalanced data sets learning. In International Conference on Intelligent Computing, pp.878-887.")
+  st.markdown("1. **He, H. and Garcia, E. A. (2008)** Learning from imbalanced data. IEEE Transactions on Knowledge and Data Engineering, 20(1), pp.1263-1284.")
+  st.markdown("2. **Chawla, N. V., Bowyer, K. W., Hall, L. O. and Kegelmeyer, W. P. (2002)** SMOTE: Synthetic Minority Over-sampling Technique. Journal of Artificial Intelligence Research, 16, pp.321-357.")
+  st.markdown("3. **Han, H., Wang, W. and Mao, B. (2005).** Borderline-SMOTE: A new over-sampling method in imbalanced data sets learning. In International Conference on Intelligent Computing, pp.878-887.")
   
 
 def create_OverRandSampling(df, label_col, num_records):
