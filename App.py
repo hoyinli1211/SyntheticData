42b6302a22fa337efb71192ed165d4f701d2cc3a
hoyinli1211 <hoyinli.r@gmail.com>
2023-01-23 16:26:01 +0800

    Update App.py

--- App.py
+++ App.py
@@ -15,7 +15,13 @@ st.sidebar.markdown("4. Choose the synthetic data method")
 
 def note():
   st.title("Note")
-  st.markdown("This is Note 1")
+  st.subheader("What is Imbalanced Data?")
+  st.markdown("Imbalanced data is a common problem in machine learning where the classes in a dataset are not represented equally. In fraud detection, for example, the number of instances of fraud may be significantly less than the number of instances of non-fraud, leading to a biased model that is less effective at detecting fraud. There are several techniques for addressing imbalanced data, including oversampling the minority class, undersampling the majority class, and using techniques such as SMOTE (Synthetic Minority Over-sampling Technique) and ADASYN (Adaptive Synthetic Sampling). The most appropriate method will depend on the specific dataset and the problem being solved. Some reference papers on imbalanced data are:")
+  st.markdown("He, H. and Garcia, E. A. (2008). Learning from imbalanced data. IEEE Transactions on Knowledge and Data Engineering, 20(1), pp.1263-1284.")
+  st.markdown("Chawla, N. V., Bowyer, K. W., Hall, L. O. and Kegelmeyer, W. P. (2002). SMOTE: Synthetic Minority Over-sampling Technique. Journal of Artificial Intelligence Research, 16, pp.321-357.")
+  st.markdown("Han, H., Wang, W. and Mao, B. (2005). Borderline-SMOTE: A new over-sampling method in imbalanced data sets learning. In International Conference on Intelligent Computing, pp.878-887.")
+
+Regenerate response")
   st.markdown("This is Note 2")
 
 def create_OverRandSampling(df, label_col, num_records):
